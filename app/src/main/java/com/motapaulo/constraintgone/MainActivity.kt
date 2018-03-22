a17291f46db8148395a67085df445e5a0d23676c
motapaulo <motapaulo@live.com>
2018-03-21 22:59:49 -0300

    Add button visibility

--- app/src/main/java/com/motapaulo/constraintgone/MainActivity.kt
+++ app/src/main/java/com/motapaulo/constraintgone/MainActivity.kt
@@ -2,11 +2,43 @@ package com.motapaulo.constraintgone
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupButtons()
+    }
+
+    private fun setupButtons() {
+        buttonA.setOnClickListener {
+            buttonA.visibility = View.GONE
+        }
+        buttonB1.setOnClickListener {
+            buttonB1.visibility = View.GONE
+        }
+        buttonB2.setOnClickListener {
+            buttonB2.visibility = View.GONE
+        }
+        buttonB3.setOnClickListener {
+            buttonB3.visibility = View.GONE
+        }
+        buttonC.setOnClickListener {
+            buttonC.visibility = View.GONE
+        }
+
+        buttonReset.setOnClickListener {
+
+            buttonA.visibility = View.VISIBLE
+            buttonB1.visibility = View.VISIBLE
+            buttonB2.visibility = View.VISIBLE
+            buttonB3.visibility = View.VISIBLE
+            buttonC.visibility = View.VISIBLE
+        }
+
+
     }
 }
